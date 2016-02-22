3188c897d3ca0dfafb215788cf313422bab499fd
Jac0bAnderson <jake912235@gmail.com>
2016-02-22 09:34:00 -0700

    Added methods to the CTECList.cpp

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -5,7 +5,72 @@
  *      Author: jand6944
  */
 
+#include "CTECList.h"
+template<class Type>
+CTECList<Type>::CTECList()
+{
 
+}
+template<class Type>
+CTECList<Type>::~CTECList()
+{
+
+}
+template<class Type>
+int CTECList<Type>:: getSize()
+{
+
+}
+template<class Type>
+void CTECList<Type>::addToFront(Type value)
+{
+
+}
+template<class Type>
+void CTECList<Type>:: addToEnd(Type value)
+{
+
+}
+template<class Type>
+void CTECList<Type>::addAtIndex(int index, Type value)
+{
+
+}
+template<class Type>
+Type CTECList<Type>::getFront()
+{
+
+}
+template<class Type>
+Type CTECList<Type>::getEnd()
+{
+
+}
+template<class Type>
+Type CTECList<Type>::getFromIndex(int index)
+{
+
+}
+template<class Type>
+Type CTECList<Type>::removeFromFront()
+{
+
+}
+template<class Type>
+Type CTECList<Type>::removeFromEnd()
+{
+
+}
+template<class Type>
+Type CTECList<Type>::removeFromIndex(int index, Type value)
+{
+
+}
+template<class Type>
+Type CTECList<Type>:: set(index, Type value)
+{
+
+}
 
 
 
