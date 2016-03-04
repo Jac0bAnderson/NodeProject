e494ae7769079e11c9b8185880c6ffd9a34bc9ec
Jac0bAnderson <jake912235@gmail.com>
2016-03-04 09:54:34 -0700

    Added code to the addToFront and addToEnd methods.

--- src/Model/CTECList.cpp
+++ src/Model/CTECList.cpp
@@ -17,7 +17,20 @@ this->end = nullptr;
 template<class Type>
 CTECList<Type>::~CTECList()
 {
-
+	ArrayNode<Type> * deleteMe = Head;
+		for (int index = 0; index < size; index++)
+		{
+			if (deleteMe->getNext() != nullptr)
+			{
+				Head = deleteMe->getNext();
+				deleteMe->setNext(nullptr);
+			} else
+			{
+				delete deleteMe->getNext();
+				deleteMe = Head;
+			}
+		}
+		delete Head;
 }
 template<class Type>
 int CTECList<Type>:: getSize()
@@ -31,11 +44,88 @@ void CTECList<Type>::addToFront(Type value)
 	//check size >0
 		//make node point to head
 		//have head point to the new node
+	ArrayNode<Type> *first, *newNode, *last, *newItem, *count;
+	assert(size >0);
+	ArrayNode<Type> *current;
+	ArrayNode<Type> *trailCurrent;
+	ArrayNode<Type> *newNode;
+	bool found;
+	newNode = new ArrayNode<Type>;
+	newNode->info = newItem;
+	newNode->link = nullptr;
+	if(first== nullptr)
+	{
+		first = newNode;
+		last = newNode;
+		count++;
+	}
+	else
+	{
+		current = first;
+		found = false;
+		while(current != nullptr && !found)
+		{
+			if(current->info >= newItem)
+			{
+			found = true;
+			}
+			else
+			{
+				trailCurrent = current;
+				current = current->links;
+			}
+			if (current == first)
+			{
+				newNode->link = first;
+				first = newNode;
+				count++;
+			}
+
+		}
+	}
 }
 template<class Type>
 void CTECList<Type>:: addToEnd(Type value)
 {
+	ArrayNode<Type> *first, *newNode, *last, *newItem, *count;
+		assert(size >0);
+		ArrayNode<Type> *current;
+		ArrayNode<Type> *trailCurrent;
+		ArrayNode<Type> *newNode;
+		bool found;
+		newNode = new ArrayNode<Type>;
+		newNode->info = newItem;
+		newNode->link = nullptr;
+		if(first== nullptr)
+		{
+			first = newNode;
+			last = newNode;
+			count++;
+		}
+		else
+		{
+			current = first;
+			found = false;
+			while(current != nullptr && !found)
+			{
+				if(current->info >= newItem)
+				{
+				found = true;
+				}
+				else
+				{
+					trailCurrent = current;
+					current = current->links;
+				}
+				if (current == first)
+				{
+					newNode->link = first;
+					first = newNode;
+					count++;
+				}
 
+			}
+		}
 }
 template<class Type>
 void CTECList<Type>::addAtIndex(int index, Type value)
@@ -64,7 +154,12 @@ return end;
 template<class Type>
 Type CTECList<Type>::getFromIndex(int index)
 {
-
+//make sure index is not <0 and >= size
+//if index == 0 or size-1 call methods for f/e
+//make a reference to head
+//loop to index
+//update  pointer to head
+//return .getValue();
 }
 template<class Type>
 Type CTECList<Type>::removeFromFront()
